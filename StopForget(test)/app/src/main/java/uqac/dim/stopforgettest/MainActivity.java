a0818dab005abc597071003d2bd90037a7d4c6c3
MOKHTARI Yanis <yanis.mokhtari@epita.fr>
2022-04-11 18:32:15 -0400

    finiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- StopForget(test)/app/src/main/java/uqac/dim/stopforgettest/MainActivity.java
+++ StopForget(test)/app/src/main/java/uqac/dim/stopforgettest/MainActivity.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.nio.channels.Selector;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,11 +29,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         tableautest=new ArrayList<String>();
+        tableautest.add("red");
+        tableautest.add("blue");
         adapter=new ArrayAdapter<>(this,R.layout.listview,tableautest);
 
         listView=findViewById(R.id.listeview);
         listView.setAdapter(adapter);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        listView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public void onItemCheckedStateChanged(ActionMode actionMode, int i, long l, boolean b) {
+
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode actionMode) {
+
+            }
+        });
     }
 
     public void creation(View v){
