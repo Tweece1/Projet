b05af84340ef8cc5fd4be5a859c6156e90e03dbe
MOKHTARI Yanis <yanis.mokhtari@epita.fr>
2022-04-20 15:19:33 -0400

    arrangement

--- StopForget(test)/app/src/main/java/uqac/dim/stopforgettest/MainActivity.java
+++ StopForget(test)/app/src/main/java/uqac/dim/stopforgettest/MainActivity.java
@@ -2,27 +2,18 @@ package uqac.dim.stopforgettest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.view.ActionMode;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<String> listes;
+    private ArrayList<String> listes_name;
+    private ArrayList<Liste> listes;
     private ArrayAdapter<String> adapter;
     private ListView listView;
     public static DataBase database;
@@ -35,8 +26,9 @@ public class MainActivity extends AppCompatActivity {
         database=new DataBase(this);
         database.open();
 
+        listes_name=database.getAllListsName();
         listes=database.getAllLists();
-        adapter=new ArrayAdapter<>(this,R.layout.listview, listes);
+        adapter=new ArrayAdapter<>(this,R.layout.listview, listes_name);
 
         listView=findViewById(R.id.listeview);
         listView.setAdapter(adapter);
@@ -53,7 +45,9 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode==2){
             if (data.getBooleanExtra("valider?",true)) {
                 adapter.add(data.getStringExtra("titre"));
-                database.addList(new Liste(data.getStringExtra("titre")));
+                Liste l=new Liste(data.getStringExtra("titre"));
+                database.addList(l);
+                listes.add(l);
             }
         }
     }
