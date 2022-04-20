a3a45e47b3f515a0741e31ea544ec438787956a3
Jérémy Bessière <jeremy.bessiere@epita.fr>
2022-04-20 15:53:07 -0400

    maj xml

--- StopForget(test)/app/src/main/java/uqac/dim/stopforgettest/MainActivity.java
+++ StopForget(test)/app/src/main/java/uqac/dim/stopforgettest/MainActivity.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
